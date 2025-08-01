df32e83587362dd4e85947915dc3537aa1f79c5d
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-08-01 15:50:42 +0200

    chore: cleaning actions

--- demo_lab/demo_lab.setup_testing.snapshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# TEST FILE  - issue #10
-#
-
-echo '{"proxmox_node":"px-testing","vm_id":1000,"vm_snapshot_description":"base"}' | proxmox_snapshot_vm.vm_id.create_snapshot.to.jsons.sh
-echo '{"proxmox_node":"px-testing","vm_id":1111,"vm_snapshot_description":"base"}' | proxmox_snapshot_vm.vm_id.create_snapshot.to.jsons.sh
