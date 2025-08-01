8edb5fcfc8b14b914160a30c666a7d8c42a657fd
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-08-01 17:48:30 +0200

    chore: minor changes

--- demo_lab/demo_lab.setup.sh
+++ demo_lab/demo_lab.setup.sh
@@ -20,11 +20,19 @@ ADMIN_INFRASTRUCTURE_IP=(
     #
     "192.168.42.102" # admin-builder-api-devkit
     "192.168.42.101" # admin-builder-docker-registry
-    "192.168.42.100" # admin-wazuh
+    # "192.168.42.100" # admin-wazuh
     "192.168.42.120" # admin-web-api-kong
     "192.168.42.121" # admin-web-builder-api
     "192.168.42.122" # admin-web-emp
     "192.168.42.123" # admin-web-deployer-ui
+    #
+    "192.168.42.160" # student-box-01
+    #
+    "192.168.42.170" # vuln-box-00
+    "192.168.42.171" # vuln-box-01
+    "192.168.42.172" # vuln-box-02
+    "192.168.42.173" # vuln-box-03
+    "192.168.42.174" # vuln-box-04
 )
 
 for ip in "${ADMIN_INFRASTRUCTURE_IP[@]}"; do
@@ -32,8 +40,11 @@ for ip in "${ADMIN_INFRASTRUCTURE_IP[@]}"; do
     ssh-keygen -f "/home/grml/.ssh/known_hosts" -R "$ip"
 done
 
-proxmox_vm.list.to.jsons.sh | grep -vi template | grep -vi group | grep -iE "(admin-)|(testing-)" | jq -c | proxmox_vm.vm_id.stop_force.to.jsons.sh
-proxmox_vm.list.to.jsons.sh | grep -vi template | grep -vi group | grep -iE "(admin-)|(testing-)" | jq -c | proxmox_vm.vm_id.delete.to.jsons.sh
+# proxmox_vm.list.to.jsons.sh | grep -vi template | grep -vi group | grep -iE "(admin-)|(testing-)" | jq -c | proxmox_vm.vm_id.stop_force.to.jsons.sh
+# proxmox_vm.list.to.jsons.sh | grep -vi template | grep -vi group | grep -iE "(admin-)|(testing-)" | jq -c | proxmox_vm.vm_id.delete.to.jsons.sh
+
+proxmox_vm.list.to.jsons.sh | grep -vi template | grep -vi group | grep -iE "(student-)|(vuln-)" | jq -c | proxmox_vm.vm_id.stop_force.to.jsons.sh
+proxmox_vm.list.to.jsons.sh | grep -vi template | grep -vi group | grep -iE "(student-)|(vuln-)" | jq -c | proxmox_vm.vm_id.delete.to.jsons.sh
 
 ANSIBLE_STDOUT_CALLBACK=skippy ansible-playbook -i ./inventory/off_cr_42.yml \
     -l "all" \
