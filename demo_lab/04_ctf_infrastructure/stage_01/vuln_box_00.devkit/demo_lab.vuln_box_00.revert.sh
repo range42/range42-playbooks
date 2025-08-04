5adb2fbca84dcd49f064a99c71b78989a6513ea7
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-08-04 22:22:55 +0200

    chore: (#14) - structure changes

---
+++ demo_lab/04_ctf_infrastructure/stage_01/vuln_box_00.devkit/demo_lab.vuln_box_00.revert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##
+## ISSUE - 14
+##
+
+proxmox_vm.list.to.jsons.sh | grep -i vuln-box-00 | proxmox_snapshot_vm.vm_id.revert_snapshot.to.jsons.sh
+proxmox_vm.list.to.jsons.sh | grep -i vuln-box-00 | proxmox_vm.vm_id.start.to.jsons.sh
