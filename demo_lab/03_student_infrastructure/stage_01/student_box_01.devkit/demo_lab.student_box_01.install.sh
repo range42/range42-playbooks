4641be6ce000f1617c0f992ab72d00ea15fbd657
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-08-11 18:10:08 +0200

    chore: minor changes to test symlinks farm feature.

--- demo_lab/03_student_infrastructure/stage_01/student_box_01.devkit/demo_lab.student_box_01.install.sh
+++ demo_lab/03_student_infrastructure/stage_01/student_box_01.devkit/demo_lab.student_box_01.install.sh
@@ -4,9 +4,9 @@
 ## ISSUE - 14
 ##
 
-ansible-playbook -i ../../../inventory/off_cr_42.yml \
-    -l "r42.student-box-01" \
-    "../_r42_student_box_group.yml" --vault-password-file /tmp/vault/vault_pass.txt
+# ansible-playbook -i ../../../inventory/off_cr_42.yml \
+#     -l "r42.student-box-01" \
+#     "../_r42_student_box_group.yml" --vault-password-file /tmp/vault/vault_pass.txt
 
 ansible-playbook -i ../../../inventory/off_cr_42.yml \
     -l "all" \
