838dcd51f400d9c98b285d29a5be2ccbff224f45
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-09-22 09:56:09 +0200

    fix: (#15) - set local variable to upper case to avoid jinja recursion errors

--- bundles/core/proxmox/configure/default/vms/start-stop-pause-resume-vms-admin/test.sh
+++ bundles/core/proxmox/configure/default/vms/start-stop-pause-resume-vms-admin/test.sh
@@ -3,6 +3,6 @@
 # echo $TMP_RANGE42_ANSIBLE_INVENTORY_DIR ; exit 1 
 
 ansible-playbook -i "$TMP_RANGE42_ANSIBLE_INVENTORY_DIR/off_cr_42.yml"  \
-	-l "r42_vuln_box_group" \
     "./main.yml" \
     --vault-password-file /tmp/vault/vault_pass.txt
+	# -l "r42_vuln_box_group" \
