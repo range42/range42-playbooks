b5d7b9c56cdafad0e0e7d93f5ff67fedc872e5fa
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2026-03-30 23:48:52 +0200

    fix: admin-infra (#22) - DNS typo fix, vault path migration, wazuh compat (16 files)

--- scenarios/demo_lab/02_admin_infrastructure/stage_00/r42_admin_group.devkit/demo_lab.r42_admin_group.install.sh
+++ scenarios/demo_lab/02_admin_infrastructure/stage_00/r42_admin_group.devkit/demo_lab.r42_admin_group.install.sh
@@ -4,6 +4,6 @@
 ## ISSUE - 14
 ##
 
-ansible-playbook -i ../../../inventory/inventory_default.yml \
+ansible-playbook -i "${RANGE42_ANSIBLE_ROLES__INVENTORY_DIR}/inventory_default.yml" \
     -l "all" \
-    "../_r42_admin_group.yml" --vault-password-file /tmp/vault/vault_pass.txt
+    "../_r42_admin_group.yml" --vault-password-file "${RANGE42_VAULT_PASSWORD_FILE:?RANGE42_VAULT_PASSWORD_FILE is not set — run: range42-context use <codename> <scenario>}"
