e0b7d936a785f135a3142d0699f7b5ba9c100e92
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2026-05-21 12:28:31 +0200

    fix(scenarios): patch stale refs in bs4, bs6, demo_lab, _init_lab (#55)

--- scenarios/blank_scenario_6_subnets/01_admin_infrastructure/stage_01/bs6_admin_wazuh.devkit/blank_scenario_6_subnets.bs6_admin_wazuh.revert.sh
+++ scenarios/blank_scenario_6_subnets/01_admin_infrastructure/stage_01/bs6_admin_wazuh.devkit/blank_scenario_6_subnets.bs6_admin_wazuh.revert.sh
@@ -3,5 +3,5 @@
 ##
 ##
 
-echo '{"proxmox_node":"px-testing","vm_id":1000 }' | proxmox_snapshot_vm.vm_id.revert_snapshot.to.jsons.sh
-echo '{"proxmox_node":"px-testing","vm_id":1000 }' | proxmox_vm.vm_id.start.to.jsons.sh
+echo '{"proxmox_node":"px-testing","vm_id":6140 }' | proxmox_snapshot_vm.vm_id.revert_snapshot.to.jsons.sh
+echo '{"proxmox_node":"px-testing","vm_id":6140 }' | proxmox_vm.vm_id.start.to.jsons.sh
