88cfb676bc9cf66dc5b5a8be47b9b4acc8bcedbc
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2026-06-19 13:55:01 +0200

    feat: debug_scenario_b_bundles - migrate to bundle-driven shape with optional admin tier (INSTALL_WAZUH/MISP, default NO) (#120)

---
+++ scenarios/debug_scenario_b_bundles/03_debug_scenario_b_infrastructure/stage_00-vm_bootstrap/r42_debug_group.devkit/debug_scenario_b_bundles.r42_debug_group.delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while IFS= read -r line; do
+    ip="${line%% *}"
+    echo ":: REMOVE SSH KEY FOR : $ip"
+    ssh-keygen -f "$HOME/.ssh/known_hosts" -R "$ip"
+done < <(devkit_manifest.find_ips_by_role.to.text.sh "$0" "debug")
+
+proxmox_vm.list.to.jsons.sh | grep -i "dsb-vm" | jq -c | proxmox_vm.vm_id.stop.to.jsons.sh
+
+sleep 2
+
+proxmox_vm.list.to.jsons.sh | grep -i "dsb-vm" | jq -c | proxmox_vm.vm_id.delete.to.jsons.sh
